c4c323f4c549cc191b83c43f21075fecfc5d14d6
andyhftang <andyhftang@gmail.com>
2024-05-15 15:14:12 -0400

    Update code.java

--- code.java
+++ code.java
@@ -1,57 +1,74 @@
-import com.jcraft.jsch.ChannelSftp;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.Session;
-import com.jcraft.jsch.SftpException;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StsAssumeRoleCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
+import software.amazon.awssdk.services.s3.model.S3Object;
+import software.amazon.awssdk.services.sts.StsClient;
+import software.amazon.awssdk.services.sts.model.AssumeRoleRequest;
+import software.amazon.awssdk.services.sts.model.AssumeRoleResponse;
 
-import java.io.ByteArrayInputStream;
-import java.util.Vector;
-
-public class SftpUtil {
-
-    public static String listFiles(String host, String port, String privateKey) {
-        Session session = null;
-        ChannelSftp channelSftp = null;
-
-        try {
-            JSch jsch = new JSch();
-            byte[] privateKeyBytes = privateKey.getBytes();
-            jsch.addIdentity("privateKey", privateKeyBytes, null, null);
-
-            session = jsch.getSession("username", host, Integer.parseInt(port));
-            session.setConfig("StrictHostKeyChecking", "no");
-
-            session.connect();
-
-            channelSftp = (ChannelSftp) session.openChannel("sftp");
-            channelSftp.connect();
-
-            Vector<ChannelSftp.LsEntry> fileList = channelSftp.ls(".");
-
-            StringBuilder fileNames = new StringBuilder();
-            for (ChannelSftp.LsEntry entry : fileList) {
-                fileNames.append(entry.getFilename()).append("\n");
-            }
-
-            return fileNames.toString();
-        } catch (JSchException | SftpException e) {
-            return "Error: " + e.getMessage();
-        } finally {
-            if (channelSftp != null) {
-                channelSftp.disconnect();
-            }
-            if (session != null) {
-                session.disconnect();
-            }
-        }
-    }
+public class S3RoleAssumer {
 
     public static void main(String[] args) {
-        String host = "your_sftp_host";
-        String port = "22";  // Default SFTP port
-        String privateKey = "your_private_key_content";
+        String roleArn = "arn:aws:iam::123456789012:role/example-role";
+        String bucketName = "example-bucket";
+        listS3BucketObjects(roleArn, bucketName);
+    }
 
-        String fileList = listFiles(host, port, privateKey);
-        System.out.println(fileList);
+    /**
+     * Method to assume a role and list objects in the specified S3 bucket.
+     *
+     * @param roleArn    The ARN of the role to be assumed.
+     * @param bucketName The name of the S3 bucket.
+     */
+    public static void listS3BucketObjects(String roleArn, String bucketName) {
+        Region region = Region.US_EAST_1; // Specify your region
+
+        // Create an STS client
+        StsClient stsClient = StsClient.builder()
+                                       .region(region)
+                                       .build();
+
+        // Assume the role
+        AssumeRoleRequest roleRequest = AssumeRoleRequest.builder()
+                                                         .roleArn(roleArn)
+                                                         .roleSessionName("S3RoleSession")
+                                                         .build();
+
+        AssumeRoleResponse roleResponse = stsClient.assumeRole(roleRequest);
+
+        AwsBasicCredentials temporaryCredentials = AwsBasicCredentials.create(
+                roleResponse.credentials().accessKeyId(),
+                roleResponse.credentials().secretAccessKey(),
+                roleResponse.credentials().sessionToken()
+        );
+
+        StsAssumeRoleCredentialsProvider credentialsProvider = StsAssumeRoleCredentialsProvider.builder()
+                .stsClient(stsClient)
+                .refreshRequest(roleRequest)
+                .build();
+
+        // Create an S3 client with the assumed role credentials
+        S3Client s3Client = S3Client.builder()
+                                    .region(region)
+                                    .credentialsProvider(credentialsProvider)
+                                    .build();
+
+        // List objects in the specified S3 bucket
+        ListObjectsV2Request listObjectsRequest = ListObjectsV2Request.builder()
+                                                                      .bucket(bucketName)
+                                                                      .build();
+
+        ListObjectsV2Response listObjectsResponse = s3Client.listObjectsV2(listObjectsRequest);
+
+        for (S3Object s3Object : listObjectsResponse.contents()) {
+            System.out.println(" - " + s3Object.key());
+        }
+
+        // Close the S3 client
+        s3Client.close();
     }
 }
