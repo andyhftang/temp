bed702a67a2c1050761edeb5507cb2b2f0fd9689
andyhftang <andyhftang@gmail.com>
2024-05-15 17:20:14 -0400

    Update code.java

--- code.java
+++ code.java
@@ -1,74 +1,35 @@
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.StsAssumeRoleCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
-import software.amazon.awssdk.services.s3.model.S3Object;
-import software.amazon.awssdk.services.sts.StsClient;
-import software.amazon.awssdk.services.sts.model.AssumeRoleRequest;
-import software.amazon.awssdk.services.sts.model.AssumeRoleResponse;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.blob.models.BlobItem;
 
-public class S3RoleAssumer {
+public class AzureBlobStorageLister {
 
     public static void main(String[] args) {
-        String roleArn = "arn:aws:iam::123456789012:role/example-role";
-        String bucketName = "example-bucket";
-        listS3BucketObjects(roleArn, bucketName);
+        String connectionString = "your-connection-string";
+        String containerName = "your-container-name";
+
+        listBlobsInContainer(connectionString, containerName);
     }
 
     /**
-     * Method to assume a role and list objects in the specified S3 bucket.
+     * List all blobs in the specified container.
      *
-     * @param roleArn    The ARN of the role to be assumed.
-     * @param bucketName The name of the S3 bucket.
+     * @param connectionString The connection string for the Azure Blob Storage account.
+     * @param containerName    The name of the container to list blobs from.
      */
-    public static void listS3BucketObjects(String roleArn, String bucketName) {
-        Region region = Region.US_EAST_1; // Specify your region
+    public static void listBlobsInContainer(String connectionString, String containerName) {
+        // Create a BlobServiceClient using the connection string
+        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
+                .connectionString(connectionString)
+                .buildClient();
 
-        // Create an STS client
-        StsClient stsClient = StsClient.builder()
-                                       .region(region)
-                                       .build();
+        // Get the container client
+        BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient(containerName);
 
-        // Assume the role
-        AssumeRoleRequest roleRequest = AssumeRoleRequest.builder()
-                                                         .roleArn(roleArn)
-                                                         .roleSessionName("S3RoleSession")
-                                                         .build();
-
-        AssumeRoleResponse roleResponse = stsClient.assumeRole(roleRequest);
-
-        AwsBasicCredentials temporaryCredentials = AwsBasicCredentials.create(
-                roleResponse.credentials().accessKeyId(),
-                roleResponse.credentials().secretAccessKey(),
-                roleResponse.credentials().sessionToken()
-        );
-
-        StsAssumeRoleCredentialsProvider credentialsProvider = StsAssumeRoleCredentialsProvider.builder()
-                .stsClient(stsClient)
-                .refreshRequest(roleRequest)
-                .build();
-
-        // Create an S3 client with the assumed role credentials
-        S3Client s3Client = S3Client.builder()
-                                    .region(region)
-                                    .credentialsProvider(credentialsProvider)
-                                    .build();
-
-        // List objects in the specified S3 bucket
-        ListObjectsV2Request listObjectsRequest = ListObjectsV2Request.builder()
-                                                                      .bucket(bucketName)
-                                                                      .build();
-
-        ListObjectsV2Response listObjectsResponse = s3Client.listObjectsV2(listObjectsRequest);
-
-        for (S3Object s3Object : listObjectsResponse.contents()) {
-            System.out.println(" - " + s3Object.key());
+        // List all blobs in the container
+        for (BlobItem blobItem : containerClient.listBlobs()) {
+            System.out.println("Blob name: " + blobItem.getName());
         }
-
-        // Close the S3 client
-        s3Client.close();
     }
 }
